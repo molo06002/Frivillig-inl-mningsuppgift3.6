8e5651bcfda0744d8aeafbece3433583946ae31b
molo06002 <molo06002@E-PW0234KQ>
2023-10-04 12:18:42 +0200

    Add project files.

---
+++ Frivillig inlämningsuppgift3.6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace inlämningsuppgift3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ditt namn och efternamn: ");
+            string x = Console.ReadLine();
+            if (x.Contains("a"))
+            {
+                Console.WriteLine("Ditt namn kommer först i bokstavs ordning");
+            }
+            else
+            {
+                Console.WriteLine("Ditt namn kommer inte först i bokstavsordningen");
+            }
+        }
+    }
+}
